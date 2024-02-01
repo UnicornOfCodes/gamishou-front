9187b5177cb05f48922ffe89acfea0000ba5445c
WildCöde <brondel@protonmail.com>
2024-02-01 23:31:45 +0100

    :construction:  landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,md,mdx,ts,tsx}"],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["dark"],
+          primary: "#F36100",
+          secondary: "teal",
+        },
+      },
+    ],
   },
+
   plugins: [require("daisyui")],
 };
